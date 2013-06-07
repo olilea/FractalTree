033bb290fbd04e24df9cca271154921748534d1f
Oliver Lea <olilea3@gmail.com>
2013-06-07 12:13:51 +0100

    Working tree, needs to be more random

--- FractalTree.py
+++ FractalTree.py
@@ -9,54 +9,52 @@ class FractalTree():
 		window.exitonclick()
 	
 	def setup(self):
-		james.pu()
-		alice.pu()
-		james.speed(10)
-		alice.speed(10)
-		james.pencolor("blue")
-		alice.pencolor("blue")
-		james.setpos(0, -100)
-		james.pd()
-		james.setpos(0, 0)
-		james.pu()
-		self.drawTree(6, 65.0, 0, 0)
-		self.drawTree(6, 65.0, 0, 0)
+		self.drawTree(2, 65.0, 0, 0, 30)
+		self.drawTree(5, 65.0, 0, 0, 30)
 		
-	def drawTree(self, n, length, x, y):
+	def drawTree(self, n, length, x, y, prevAngle):
 		if n == 0:
 			return
 		
-		angle1 = 15
-		
 		james.setpos(x, y)
-		alice.setpos(x, y)
 		
 		james.pd()
-		alice.pd()
 		
-		james.left(2* angle1)
+		angle1 = prevAngle + 30
+		angle2 = prevAngle - 30
+		
+		james.setheading(angle1)
+		
 		james.forward(length)
-		alice.right(2* angle1)
-		alice.forward(length)
-		
-		james.pu()
-		alice.pu()
-		
-		length *= 0.65
-		n -= 1
-		
 		x1 = james.pos()[0]
 		y1 = james.pos()[1]
-		x2 = alice.pos()[0]
-		y2 = alice.pos()[1]
 		
-		self.drawTree(n, length, x1, y1)
-		self.drawTree(n, length, x2, y2)
+		james.backward(length)
+		james.setheading(angle2)
+		
+		james.forward(length)
+		x2 = james.pos()[0]
+		y2 = james.pos()[1]
+		
+		james.pu()
+		self.drawTree(n - 1, length * 0.65, x1, y1, angle1)
+		self.drawTree(n - 1, length * 0.65, x2, y2, angle2)
+		
     
-
-#james is the name of the turtle
-james = turtle.Turtle()
-alice = turtle.Turtle()
 window = turtle.Screen()
+
+james = turtle.Turtle()
+
+turtle.mode("logo")
+
+james.speed(10)
+james.color("blue")
+
+james.pu()
+james.backward(100)
+james.pd()
+james.forward(100)
+james.pu()
+
 
 tree = FractalTree()
