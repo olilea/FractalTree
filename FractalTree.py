7db0d87e38480e99cb4ec95f518ab849da59158e
Oliver Lea <olilea3@gmail.com>
2013-06-07 13:14:48 +0100

    Improved and randomised. Pen not consistent

--- FractalTree.py
+++ FractalTree.py
@@ -9,25 +9,30 @@ class FractalTree():
 		window.exitonclick()
 	
 	def setup(self):
-		self.drawTree(2, 65.0, 0, 0, 30)
-		self.drawTree(5, 65.0, 0, 0, 30)
+		self.drawTree(5, 100, 0, -200, 0)
 		
 	def drawTree(self, n, length, x, y, prevAngle):
 		if n == 0:
 			return
+		if (n > 0) and (james.pensize() >= 1):
+			james.pensize(james.pensize() - 0.5)
+		
 		
 		james.setpos(x, y)
 		
 		james.pd()
 		
-		angle1 = prevAngle + 30
-		angle2 = prevAngle - 30
+		angle1 = prevAngle + random.randrange(20, 40)
+		angle2 = prevAngle - random.randrange(20, 40)
 		
 		james.setheading(angle1)
 		
 		james.forward(length)
 		x1 = james.pos()[0]
 		y1 = james.pos()[1]
+		
+		if n == 1:
+			james.dot(5, "green")
 		
 		james.backward(length)
 		james.setheading(angle2)
@@ -36,9 +41,12 @@ class FractalTree():
 		x2 = james.pos()[0]
 		y2 = james.pos()[1]
 		
+		if n == 1:
+			james.dot(5, "green")
+		
 		james.pu()
-		self.drawTree(n - 1, length * 0.65, x1, y1, angle1)
-		self.drawTree(n - 1, length * 0.65, x2, y2, angle2)
+		self.drawTree(n - 1, random.uniform(0.75,0.9)*length, x1, y1, angle1)
+		self.drawTree(n - 1, random.uniform(0.75,0.9)*length, x2, y2, angle2)
 		
     
 window = turtle.Screen()
@@ -49,12 +57,12 @@ turtle.mode("logo")
 
 james.speed(10)
 james.color("blue")
+james.pensize(6)
 
 james.pu()
-james.backward(100)
+james.backward(300)
 james.pd()
 james.forward(100)
 james.pu()
-
 
 tree = FractalTree()
