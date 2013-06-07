14e875c211f1e24ef77336f33e04807aebe0b88d
Oliver Lea <olilea3@gmail.com>
2013-06-07 01:08:24 +0100

    Added setup and two Turtle instances

--- FractalTree.py
+++ FractalTree.py
@@ -4,24 +4,59 @@ import random
 
 class FractalTree():
 
-	def __init__(self, james, window):
+	def __init__(self):
 		self.setup()
 		window.exitonclick()
 	
 	def setup(self):
-		james.left(90)
-		james.color("blue")
-		james.penup()
-		james.backward(150)
-		james.pendown()
-		self.drawTree(5)
+		james.pu()
+		alice.pu()
+		james.speed(10)
+		alice.speed(10)
+		james.pencolor("blue")
+		alice.pencolor("blue")
+		james.setpos(0, -100)
+		james.pd()
+		james.setpos(0, 0)
+		james.pu()
+		self.drawTree(6, 65.0, 0, 0)
+		self.drawTree(6, 65.0, 0, 0)
 		
-	def drawTree(self, n):
-		return
+	def drawTree(self, n, length, x, y):
+		if n == 0:
+			return
+		
+		angle1 = 15
+		
+		james.setpos(x, y)
+		alice.setpos(x, y)
+		
+		james.pd()
+		alice.pd()
+		
+		james.left(2* angle1)
+		james.forward(length)
+		alice.right(2* angle1)
+		alice.forward(length)
+		
+		james.pu()
+		alice.pu()
+		
+		length *= 0.65
+		n -= 1
+		
+		x1 = james.pos()[0]
+		y1 = james.pos()[1]
+		x2 = alice.pos()[0]
+		y2 = alice.pos()[1]
+		
+		self.drawTree(n, length, x1, y1)
+		self.drawTree(n, length, x2, y2)
     
 
 #james is the name of the turtle
 james = turtle.Turtle()
+alice = turtle.Turtle()
 window = turtle.Screen()
 
-tree = FractalTree(james, window)
+tree = FractalTree()
